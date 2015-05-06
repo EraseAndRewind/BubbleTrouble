331b10daeebe8317873180369da890546bf25c0e
EraseAndRewind <a.demboski@yahoo.com>
2015-05-06 10:53:11 +0200

    AddBubble

--- BubbleTrouble/Form1.cs
+++ BubbleTrouble/Form1.cs
@@ -12,9 +12,56 @@ namespace BubbleTrouble
 {
     public partial class Form1 : Form
     {
+        private ObjectDoc objectDoc;
+        private Timer timer;
+        private int left;
+        private int top;
+        private int width;
+        private int height;
+            
         public Form1()
         {
             InitializeComponent();
+            objectDoc = new ObjectDoc();
+            this.DoubleBuffered = true;
+            timer = new Timer();
+            timer.Interval = 50;
+            timer.Tick += new EventHandler(timer_Tick);
+            newGame();
+            
         }
+
+        void newGame()
+        {
+            left = 0;
+            top = 0;
+            width = this.width;
+            height = this.height;
+
+            Point point = new Point(10, 10);
+            objectDoc.spawnBubble(new Bubble(point, 20, Color.Red, 45));
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            objectDoc.moveObjects(left, top, width, height);
+            objectDoc.checkCollision();
+            Invalidate(true);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.White);
+            objectDoc.Draw(e.Graphics);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+
+
+
     }
 }
